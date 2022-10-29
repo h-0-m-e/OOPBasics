244452585c8fc607f2931c5ed5fc0d373a512355
Nikita Vasilev <thisish000me@gmail.com>
2022-10-29 20:42:39 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,61 @@
+import java.util.Objects
+
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val publisherId: Int,
+    val publishDate: Long,
+    val friendsOnly: Boolean,
+    val text: String,
+//    val reposts: Objects,
+    val views: Int,
+    val likes: Int
+)
+
+data class Reposts(
+    val count: Int,
+    val userReposted: Boolean
+)
+
+object WallService {
+
+    private var idCounter = 1
+    private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post{
+        posts += post.copy(id = idCounter++)
+        return posts.last()
+    }
+
+    fun update(updatedPost: Post): Boolean {
+        for((index, post) in posts.withIndex()){
+            if (updatedPost.id == post.id){
+                posts[index] = updatedPost.copy()
+                return true
+            }
+        }
+        return false
+    }
+
+    fun clear(){
+        posts = emptyArray()
+        idCounter = 1
+    }
+
+    fun printAll(){
+        for(post in posts){
+            println(post)
+        }
+    }
+}
+
+fun main() {
+    val post = Post(1,9923,9923,9923,20222610,false,"Text Sample",views = 0, likes = 0)
+    WallService.add(post)
+    WallService.add(Post(5,23,12,22,20212109,false,"Nothing to read here", views = 0, likes = 0))
+    WallService.printAll()
+    val newPost = Post(1,9923,9923,9923,20222610,false,"NEW Text Sample",views = 0, likes = 0)
+    WallService.update(newPost)
+    WallService.printAll()
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,54 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class WallServiceTest {
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun add_IdChanged() {
+
+        val post = Post(
+            0,
+            9923,
+            9923,
+            9923,
+            20222610,
+            false,
+            "Text Sample",
+            views = 0,
+            likes = 0
+        )
+        val result = WallService.add(post).id
+
+        assertNotEquals(0, result)
+    }
+
+    @Test
+    fun update_PostExist(){
+        val post = Post(1,9923,9923,9923,20222610,false,"Text Sample",views = 0, likes = 0)
+        WallService.add(post)
+        val updatedPost = Post(post.id,9923,9923,9923,20222810,false,"Updated Text",views = 0, likes = 0)
+
+        val result = WallService.update(updatedPost)
+
+        assertEquals(true,result)
+    }
+
+    @Test
+    fun update_PostDoNotExist(){
+        val post = Post(1,9923,9923,9923,20222610,false,"Text Sample",views = 0, likes = 0)
+        WallService.add(post)
+        val updatedPost = Post(19,9923,9923,9923,20222810,false,"Updated Text",views = 0, likes = 0)
+
+        val result = WallService.update(updatedPost)
+
+        assertNotEquals(true,result)
+    }
+
+}
